b6d8701710bf75e6033da90fb3e7a1d61d798e13
SarimIrfan2015 <sarimirfan843@gmail.com>
2025-12-11 19:12:26 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+document.write("Q1: 5 + 3")
+
+var a = 5
+var b = 3
+var c = a++ + a++ + a++
+
+alert("Subtraction: 10 - 5 = " + (10 - 5));
+
+alert("Multiplication: 4 + 3 = " + (4 * 3));
+
+alert("Division: 20 ÷ 4 = " + (20 / 4));
+
+alert("Modulus: 17 * 3 = " + (17 % 3));
+
+var num;
+
+document.write("Value after variable declaration is: " + num + "<br>");
+
+num = 5;
+
+document.write("Initial value: " + num + "<br>");
+
+num++;
+
+document.write("Value after increment is: " + num + "<br>");
+
+num = num + 7;
+
+document.write("Value after adding 7 is: " + num + "<br>");
